db7cb45d3cf313ce3401fd35ba41bb0e5dda89f1
Kaysic <michaeldaq@mail.com>
2017-02-11 13:29:37 -0500

    added Cars model for database

---
+++ models/Cars.js
@@ -0,0 +1,44 @@
+module.exports = function (sequelize, DataTypes) {
+	return sequelize.define("Car", {
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		make: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				isAlpha: true, 
+				isEmpty: false
+			}
+		},
+		model: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				isAlpha: true,
+				isEmpty: false
+			}
+		},
+		year: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate: {
+				isNumeric: true,
+				isEmpty: false
+			}
+		},
+		color: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				isAlpha: true
+			}
+		}
+
+
+	})
+
+};
